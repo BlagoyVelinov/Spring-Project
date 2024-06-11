93a78c7c81856743d10d00ded4e9257768cc35b8
Blagoy <blagoy1234@gmail.com>
2024-06-11 12:27:56 +0300

    Initial commit

---
+++ src/main/java/bg/softuni/mycinematicketsapp/MyCinemaTicketsAppApplication.java
@@ -0,0 +1,13 @@
+package bg.softuni.mycinematicketsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCinemaTicketsAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyCinemaTicketsAppApplication.class, args);
+    }
+
+}
---
+++ src/main/java/bg/softuni/mycinematicketsapp/controllers/UserController.java
@@ -0,0 +1,18 @@
+package bg.softuni.mycinematicketsapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+
+    @GetMapping("/login")
+    public String getLogin() {
+        return "login";
+    }
+
+    @GetMapping("/register")
+    public String getRegister() {
+        return "register";
+    }
+}
