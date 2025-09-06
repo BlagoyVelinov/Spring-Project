aa06ddbebe998b691adfe3c7f61a5e774a817b6d
Blagoy <blagoy1234@gmail.com>
2025-09-06 10:03:12 +0300

    Fix user validation

--- src/main/java/bg/softuni/mycinematicketsapp/web/UserController.java
+++ src/main/java/bg/softuni/mycinematicketsapp/web/UserController.java
@@ -1,10 +1,8 @@
 package bg.softuni.mycinematicketsapp.web;
 
-import bg.softuni.mycinematicketsapp.constants.ExceptionMessages;
 import bg.softuni.mycinematicketsapp.models.dtos.UserDetailsDto;
 import bg.softuni.mycinematicketsapp.models.dtos.view.UserViewDto;
-import bg.softuni.mycinematicketsapp.models.entities.UserEntity;
-import bg.softuni.mycinematicketsapp.models.enums.UserRoleEnum;
+import bg.softuni.mycinematicketsapp.services.SecurityService;
 import bg.softuni.mycinematicketsapp.services.UserService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
@@ -12,7 +10,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.Map;
@@ -22,9 +19,11 @@ import java.util.Map;
 public class UserController {
 
     private final UserService userService;
+    private final SecurityService securityService;
 
-    public UserController(UserService userService) {
+    public UserController(UserService userService, SecurityService securityService) {
         this.userService = userService;
+        this.securityService = securityService;
     }
 
     @PreAuthorize("hasRole('ADMINISTRATOR')")
@@ -40,7 +39,7 @@ public class UserController {
 
     @GetMapping("/user/{id}")
     public ResponseEntity<UserDetailsDto> getUserById(@PathVariable long id, Authentication authentication) {
-        validateCurrentUser(id, authentication);
+       securityService.validateCurrentUser(id, authentication);
 
         return ResponseEntity.ok(userService.getUserDetailsDtoById(id));
     }
@@ -49,7 +48,7 @@ public class UserController {
     public ResponseEntity<UserDetailsDto> editUserById(@PathVariable long id,
                                                        Authentication authentication,
                                                        @Valid @RequestBody UserDetailsDto userDetails) {
-        validateCurrentUser(id, authentication);
+        securityService.validateCurrentUser(id, authentication);
 
         return ResponseEntity.ok(userService.editUserDetailsDtoById(id, userDetails));
     }
@@ -58,7 +57,7 @@ public class UserController {
     public ResponseEntity<?> editProfileImageById(@PathVariable long id,
                                                   Authentication authentication,
                                                   @RequestBody String imageUrl) {
-        validateCurrentUser(id, authentication);
+        securityService.validateCurrentUser(id, authentication);
 
         String response = userService.editProfilePhotoById(id, imageUrl);
 
@@ -67,7 +66,7 @@ public class UserController {
 
     @DeleteMapping("/delete-user/{id}")
     public ResponseEntity<?> deactivateUserById(@PathVariable long id, Authentication authentication) {
-        validateCurrentUser(id, authentication);
+        securityService.validateCurrentUser(id, authentication);
 
         boolean isDeleted = userService.deactivateCurrentUserById(id);
 
@@ -77,22 +76,6 @@ public class UserController {
             return ResponseEntity
                     .status(HttpStatus.BAD_REQUEST)
                     .body(Map.of("error", "User could not be deleted or is not active"));
-        }
-    }
-
-    private void validateCurrentUser(long id, Authentication authentication) {
-        String currentUsername = authentication.getName();
-        UserEntity currentUser = userService.getUserByUsername(currentUsername);
-
-        boolean isAdmin = currentUser.getRoles()
-                .stream()
-                .anyMatch(userRole -> userRole
-                        .getRole()
-                        .name()
-                        .equals(UserRoleEnum.ADMINISTRATOR.name()));
-
-        if (!isAdmin && currentUser.getId() != id) {
-            throw new ResponseStatusException(HttpStatus.FORBIDDEN, ExceptionMessages.NOT_ADMIN_RIGHTS);
         }
     }
 }
