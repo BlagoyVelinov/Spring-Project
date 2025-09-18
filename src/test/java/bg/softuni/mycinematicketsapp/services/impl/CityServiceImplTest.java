668b7d91e0b1e4afd78ae845ccf714fb174b4664
Blagoy <blagoy1234@gmail.com>
2025-09-18 16:59:24 +0300

    Add unit test for init cities in DB

--- src/test/java/bg/softuni/mycinematicketsapp/services/impl/CityServiceImplTest.java
+++ src/test/java/bg/softuni/mycinematicketsapp/services/impl/CityServiceImplTest.java
@@ -1,2 +1,35 @@
-package bg.softuni.mycinematicketsapp.services.impl;public class CityServiceImplTest {
+package bg.softuni.mycinematicketsapp.services.impl;
+
+import bg.softuni.mycinematicketsapp.repository.CityRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class CityServiceImplTest {
+
+    @Mock
+    private CityRepository cityRepository;
+
+    @InjectMocks
+    private CityServiceImpl cityService;
+
+    @Test
+    void testInitCitiesNamesInDb_Success() {
+        when(cityRepository.count()).thenReturn(0L);
+
+        cityService.initCitiesNamesInDb();
+
+        verify(cityRepository, times(1)).saveAll(anySet());
+    }
+
+    @AfterEach
+    void tearDown() {
+        reset(cityRepository);
+    }
 }
