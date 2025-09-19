15ad377e2324fa00c51767ae2f0e624c78409a66
Blagoy <blagoy1234@gmail.com>
2025-09-19 16:55:26 +0300

    Unit test for get all schools offers

--- src/test/java/bg/softuni/mycinematicketsapp/services/impl/OfferServiceImplTest.java
+++ src/test/java/bg/softuni/mycinematicketsapp/services/impl/OfferServiceImplTest.java
@@ -146,6 +146,26 @@ public class OfferServiceImplTest {
         Mockito.verify(offerRepository, Mockito.times(1)).findAll();
     }
 
+    @Test
+    void testGetAllSchoolsOffers() {
+        Offer offer1 = getOffer(ConstantTest.TEST_OFFER_TITLE, OfferType.FOR_THE_SCHOOLS, 1L);
+        Offer offer2 = getOffer("Test title 2", OfferType.CINEMA_OFFERS, 2L);
+        Offer offer3 = getOffer("Test title 3", OfferType.FOR_THE_SCHOOLS, 3L);
+
+        List<Offer> offers = List.of(offer1, offer2, offer3);
+        when(offerRepository.findAll()).thenReturn(offers);
+
+        Mockito.when(modelMapper.map(offer1, OfferViewDto.class)).thenReturn(new OfferViewDto());
+        Mockito.when(modelMapper.map(offer3, OfferViewDto.class)).thenReturn(new OfferViewDto());
+
+        List<OfferViewDto> cinemaOffers = offerService.getAllSchoolsOffers();
+
+        Assertions.assertNotNull(cinemaOffers);
+        Assertions.assertEquals(2, cinemaOffers.size());
+
+        Mockito.verify(offerRepository, Mockito.times(1)).findAll();
+    }
+
 
 
     private static AddOfferDto getAddOfferDto() {
